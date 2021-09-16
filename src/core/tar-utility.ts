9692bdf709366f88630f2a316c2bef1edae24677
Josh Noel <obsidize@gmail.com>
2021-09-15 21:31:42 -0500

    normalize utility function declarations

--- src/core/tar-utility.ts
+++ src/core/tar-utility.ts
@@ -8,48 +8,48 @@ export namespace TarUtility {
 	export const SECTOR_SIZE = 512;
 	export const USTAR_HEADER_SECTOR_TAG = 'ustar\0';
 
-	export const parseAscii = (input: Uint8Array): string => {
+	export function parseAscii(input: Uint8Array): string {
 		return String.fromCharCode.apply(null, Array.from(input));
-	};
+	}
 
-	export const isNumber = (value: any): boolean => {
+	export function isNumber(value: any): boolean {
 		return typeof value === 'number' && !Number.isNaN(value);
-	};
+	}
 
-	export const parseIntSafe = (value: any, radix: number = 10, defaultValue: number = 0): number => {
+	export function parseIntSafe(value: any, radix: number = 10, defaultValue: number = 0): number {
 		if (isNumber(value)) return value;
 		const parsed = parseInt(value, radix);
 		return isNumber(parsed) ? parsed : defaultValue;
-	};
+	}
 
-	export const sliceFieldBuffer = (field: TarHeaderField, input: Uint8Array, offset: number = 0): Uint8Array => {
+	export function sliceFieldBuffer(field: TarHeaderField, input: Uint8Array, offset: number = 0): Uint8Array {
 		const absoluteOffset = field.offset + offset;
 		return input.slice(absoluteOffset, absoluteOffset + field.size);
-	};
+	}
 
-	export const parseAsciiOctalNumberField = (input: Uint8Array): number => {
+	export function parseAsciiOctalNumberField(input: Uint8Array): number {
 		return parseIntSafe(parseAscii(input), 8);
-	};
+	}
 
-	export const parseNumberField = (input: Uint8Array): number => {
+	export function parseNumberField(input: Uint8Array): number {
 		return new DataView(input).getFloat64(0);
-	};
+	}
 
-	export const readFieldValue = (field: TarHeaderField, input: Uint8Array, offset?: number): any => {
+	export function readFieldValue(field: TarHeaderField, input: Uint8Array, offset?: number): any {
 		return parseFieldValueByType(field.type, sliceFieldBuffer(field, input, offset));
-	};
+	}
 
-	export const isUstarSector = (input: Uint8Array, offset: number): boolean => {
+	export function isUstarSector(input: Uint8Array, offset: number): boolean {
 		return readFieldValue(TarHeaderFieldDefinition.ustarIndicator(), input, offset) === USTAR_HEADER_SECTOR_TAG;
-	};
+	}
 
-	export const advanceSectorOffset = (currentOffset: number, maxOffset: number): number => {
+	export function advanceSectorOffset(currentOffset: number, maxOffset: number): number {
 		const nextOffsetRaw = Math.round(currentOffset) + SECTOR_SIZE;
 		const diffToBoundary = nextOffsetRaw % SECTOR_SIZE;
 		return Math.min(maxOffset, nextOffsetRaw - diffToBoundary);
-	};
+	}
 
-	export const parseFieldValueByType = (fieldType: TarHeaderFieldType, input: Uint8Array): any => {
+	export function parseFieldValueByType(fieldType: TarHeaderFieldType, input: Uint8Array): any {
 		switch (fieldType) {
 			case TarHeaderFieldType.ASCII:
 				return parseAscii(input);
@@ -58,5 +58,5 @@ export namespace TarUtility {
 			default:
 				return parseNumberField(input);
 		}
-	};
+	}
 }
