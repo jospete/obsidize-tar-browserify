1ee815cf744eab1fa0a1e02dce2b22045d3a24bd
Josh Noel <obsidize@gmail.com>
2021-09-22 13:22:21 -0500

    fix spec file name

--- tests/tar-header-field-definition.spec.ts
+++ tests/tar-header-field-definition.spec.ts
@@ -1,6 +1,6 @@
 import { TarHeaderFieldDefinition, TarHeaderFieldType } from '../src';
 
-describe('TarHeader', () => {
+describe('TarHeaderFieldDefinition', () => {
 
 	it('is a collection of header metadata options to streamline tar header parsing', () => {
 		const fileNameMetadata = TarHeaderFieldDefinition.fileName();
