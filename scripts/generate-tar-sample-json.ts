102b34b62e63f61ccdd3a09cbdc3c041180cb8e2
= <obsidize@gmail.com>
2024-11-01 22:26:04 -0500

    fix sample json generator to see wtf is going on with this new ArchiveReader class

--- scripts/generate-tar-sample-json.ts
+++ scripts/generate-tar-sample-json.ts
@@ -1,13 +1,12 @@
 #! /usr/bin/env node
 
 import { mkdirpSync, readFileSync, writeFileSync } from 'fs-extra';
-import { Tarball } from '../dist';
+import { Archive } from '../dist';
 
 async function main() {
 	const tarFilePath = process.argv[2] || './dev-assets/tarball-sample/packed/node-tar-sample.tar';
 	const sampleTarBuffer = readFileSync(tarFilePath);
-	const tarball = new Tarball(sampleTarBuffer);
-	const {entries} = tarball;
+	const entries = await Archive.extract(sampleTarBuffer);
 
 	mkdirpSync('./tmp');
 	writeFileSync('./tmp/tarball-metadata.txt', JSON.stringify(entries, null, '\t'), 'utf-8');
