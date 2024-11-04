0ef43c42bfc7191f650b7469b10b864b2330d215
= <obsidize@gmail.com>
2024-11-04 11:42:36 -0600

    make coverage mandatory in jest config for tests

--- jest.config.ts
+++ jest.config.ts
@@ -36,15 +36,22 @@ const config: Config = {
   // coverageProvider: "babel",
 
   // A list of reporter names that Jest uses when writing coverage reports
-  // coverageReporters: [
-  //   "json",
-  //   "text",
-  //   "lcov",
-  //   "clover"
-  // ],
+  coverageReporters: [
+    "json",
+    // "text",
+    "lcov",
+    // "clover"
+  ],
 
   // An object that configures minimum threshold enforcement for coverage results
-  // coverageThreshold: undefined,
+  coverageThreshold: {
+	global: {
+		branches: 90,
+		functions: 90,
+		lines: 90,
+		statements: 90
+	}
+  },
 
   // A path to a custom dependency extractor
   // dependencyExtractor: undefined,
