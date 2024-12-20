499d5bf895c4143055f510fc584a62625e0af839
Josh Noel <obsidize@gmail.com>
2024-12-20 11:26:48 -0600

    fix barrel exports at root of module

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { Archive } from './archive/archive';
 export { ArchiveReadError, ArchiveReader } from './archive/archive-reader';
-export { ArchiveWriter } from './archive/archive-writer';
+export { ArchiveWriter, TarEntryPredicate } from './archive/archive-writer';
 export { ArchiveContext } from './common/archive-context';
 export { AsyncUint8ArrayLike, InMemoryAsyncUint8Array } from './common/async-uint8-array';
 export { AsyncUint8ArrayBlock, AsyncUint8ArrayIterator, AsyncUint8ArrayIteratorLike, AsyncUint8ArrayIteratorOptions } from './common/async-uint8-array-iterator';
@@ -10,8 +10,9 @@ export { TarEntry, TarEntryOptions } from './entry/tar-entry';
 export { PaxTarHeader, PaxTarHeaderAttributes } from './header/pax/pax-tar-header';
 export { PaxTarHeaderKey } from './header/pax/pax-tar-header-key';
 export { PaxTarHeaderSegment } from './header/pax/pax-tar-header-segment';
-export { TarHeader } from './header/tar-header';
+export { TarHeader, TarHeaderOptions } from './header/tar-header';
 export { TarHeaderUtility } from './header/tar-header-utility';
+export { UstarHeader } from './header/ustar/ustar-header';
 export { UstarHeaderField, UstarHeaderFieldLike } from './header/ustar/ustar-header-field';
 export { UstarHeaderFieldTransform, UstarHeaderFieldTransformType } from './header/ustar/ustar-header-field-transform';
 export { UstarHeaderFieldType } from './header/ustar/ustar-header-field-type';
