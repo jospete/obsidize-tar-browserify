ea4e574bc272167c1085bf542c620b342177418a
Josh Noel <obsidize@gmail.com>
2025-07-11 00:17:56 -0500

    move ArchiveEntry definition into archive folder

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
 export { Archive } from './archive/archive';
+export { ArchiveEntry, ArchiveEntryOptions } from './archive/archive-entry';
 export { ArchiveReader, ArchiveReadError } from './archive/archive-reader';
 export { ArchiveEntryPredicate, ArchiveWriter } from './archive/archive-writer';
 export { ArchiveContext } from './common/archive-context';
@@ -6,7 +7,6 @@ export { AsyncUint8ArrayLike, InMemoryAsyncUint8Array } from './common/async-uin
 export { AsyncUint8ArrayBlock, AsyncUint8ArrayIterator, AsyncUint8ArrayIteratorLike, AsyncUint8ArrayIteratorOptions } from './common/async-uint8-array-iterator';
 export { Constants } from './common/constants';
 export { TarSerializable, TarUtility } from './common/tar-utility';
-export { ArchiveEntry, ArchiveEntryOptions } from './entry/archive-entry';
 export { PaxHeader, PaxHeaderAttributes } from './header/pax/pax-header';
 export { PaxHeaderKey } from './header/pax/pax-header-key';
 export { PaxHeaderSegment } from './header/pax/pax-header-segment';
