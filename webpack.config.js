5b9351130d89fbbbcb4d189adb9775e88ececa35
= <obsidize@gmail.com>
2025-07-11 15:26:25 -0500

    remove unnecessary tslib external

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,12 @@ const baseConfig = {
 	entry: './src/index.ts',
 	mode: 'production',
 	target: ['web', 'es5'],
-	externals: ['tslib'],
 	module: {
 		rules: [
 			{
 				test: /\.(ts|tsx)$/,
 				use: 'ts-loader',
-				exclude: [
-					/node_modules/,
-					/\.test\.ts$/,
-					/\.mock\.ts$/,
-					/src\/test\//
-				],
+				exclude: [/node_modules/, /\.test\.ts$/, /\.mock\.ts$/, /src\/test\//],
 			},
 		],
 	},
